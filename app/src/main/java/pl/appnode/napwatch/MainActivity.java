07ebb915edaa0f88f91963d864c229c92b12df9b
AnimalRationale <wojciech.mardyla@gmail.com>
2014-11-23 22:46:39 +0100

    MainActivity layout.

--- app/src/main/java/pl/appnode/napwatch/MainActivity.java
+++ app/src/main/java/pl/appnode/napwatch/MainActivity.java
@@ -1,9 +1,9 @@
 package pl.appnode.napwatch;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
