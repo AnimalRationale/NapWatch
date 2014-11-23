792582a89dea6145f2a2940ae2640b25069b18ef
AnimalRationale <wojciech.mardyla@gmail.com>
2014-11-23 23:15:23 +0100

    Initial for AlarmAdapter.

---
+++ app/src/main/java/pl/appnode/napwatch/AlarmInfo.java
@@ -0,0 +1,16 @@
+package pl.appnode.napwatch;
+
+/**
+ * Created by Monki on 2014-11-23.
+ */
+public class AlarmInfo {
+    protected String name;
+    protected String description;
+    protected String duration;
+
+
+    protected static final String NAME_PREFIX = "Name_";
+    protected static final String DESCRIPTION_PREFIX = "Description_";
+    protected static final String DURATION_PREFIX = "Duration_";
+}
+
