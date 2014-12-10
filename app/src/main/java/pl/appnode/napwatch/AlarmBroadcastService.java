ef0127242ddf7a5f11489bc0f9e7f5d9a34c806e
AnimalRationale <wojciech.mardyla@gmail.com>
2014-12-10 01:16:31 +0100

    Mockup timer minutes->seconds.

--- app/src/main/java/pl/appnode/napwatch/AlarmBroadcastService.java
+++ app/src/main/java/pl/appnode/napwatch/AlarmBroadcastService.java
@@ -13,6 +13,10 @@ public class AlarmBroadcastService extends Service {
 
     public static final String COUNTDOWN_BROADCAST = "pl.appnode.napwatch";
     Intent mBI = new Intent(COUNTDOWN_BROADCAST);
+    String mAlarmId;
+    String mAlarmName;
+    int mAlarmDuration;
+
     int mStartMode;       // indicates how to behave if the service is killed
 
     CountDownTimer mCDT = null;
@@ -21,14 +25,17 @@ public class AlarmBroadcastService extends Service {
     public void onCreate() {
         super.onCreate();
 
-
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, "Starting timer.");
-        extras = intent.getExtras();
-        mCDT = new CountDownTimer(30000, 5000) {
+
+        mAlarmId = intent.getExtras().get("AlarmId").toString();
+        mAlarmName = intent.getExtras().get("AlarmName").toString();
+        mAlarmDuration = (Integer) intent.getExtras().get("AlarmDuration");
+
+        Log.d(TAG, "Starting timer for " + mAlarmId + "/" + mAlarmName  + " with duration " + mAlarmDuration + " minutes." );
+        mCDT = new CountDownTimer(mAlarmDuration*1000, 5000) {
             @Override
             public void onTick(long millisUntilFinished) {
 
@@ -55,10 +62,10 @@ public class AlarmBroadcastService extends Service {
         super.onDestroy();
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
-    }
+//    @Override
+//    public int onStartCommand(Intent intent, int flags, int startId) {
+//        return super.onStartCommand(intent, flags, startId);
+//    }
 
     @Override
     public IBinder onBind(Intent arg0) {
