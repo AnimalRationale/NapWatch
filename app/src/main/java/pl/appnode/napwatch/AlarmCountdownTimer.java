fc750b0dde91cf362bf2492f6786f8cdd3abef3d
AnimalRationale <wojciech.mardyla@gmail.com>
2014-12-14 23:58:29 +0100

    Class for custom countdowntimers.

--- app/src/main/java/pl/appnode/napwatch/AlarmCountdownTimer.java
+++ app/src/main/java/pl/appnode/napwatch/AlarmCountdownTimer.java
@@ -1,4 +1,36 @@
 package pl.appnode.napwatch;
 
-public class AlarmCountdownTimer {
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.CountDownTimer;
+import android.util.Log;
+
+public class AlarmCountdownTimer extends CountDownTimer {
+
+    private final static String TAG = "::Service.AlarmCountdownTimer";
+    public static final String COUNTDOWN_BROADCAST = "pl.appnode.napwatch";
+    Intent mBI = new Intent(COUNTDOWN_BROADCAST);
+    int mAlarmId;
+    String mAlarmName;
+    int mAlarmDuration;
+    
+    Uri mAlert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+    Ringtone mRingtone;
+
+    @Override
+    public void onTick(long millisUntilFinished) {
+
+        Log.d(TAG, "Countdown seconds remaining: " + millisUntilFinished / 1000);
+        mBI.putExtra("AlarmID", mAlarmId);
+        mBI.putExtra("countdown", millisUntilFinished / 1000);
+        sendBroadcast(mBI);
+    }
+
+    @Override
+    public void onFinish() {
+        mRingtone.play();
+        Log.d(TAG, "Timer [" + mAlarmId + "] finished.");
+    }
 }
