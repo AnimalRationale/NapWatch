bf239479edd135e25acd1c6335266de389572648
AnimalRationale <wojciech.mardyla@gmail.com>
2015-02-21 20:13:17 +0100

    Init for app start from widget.

--- app/src/main/java/pl/appnode/napwatch/NapWatchWidgetProvider.java
+++ app/src/main/java/pl/appnode/napwatch/NapWatchWidgetProvider.java
@@ -4,4 +4,6 @@ import android.appwidget.AppWidgetProvider;
 
 public class NapWatchWidgetProvider extends AppWidgetProvider {
 
+    public static String WIDGET_BUTTON = "pl.appnode.napwatch.WIDGET_BUTTON";
+    
 }
