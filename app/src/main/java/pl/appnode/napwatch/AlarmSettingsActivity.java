0251b62f9bc76205ca35ad617fc35d0bd488a814
AnimalRationale <wojciech.mardyla@gmail.com>
2015-02-24 23:27:15 +0100

    Scaffold for activity layout.

--- app/src/main/java/pl/appnode/napwatch/AlarmSettingsActivity.java
+++ app/src/main/java/pl/appnode/napwatch/AlarmSettingsActivity.java
@@ -1,9 +1,18 @@
 package pl.appnode.napwatch;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 
 public class AlarmSettingsActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = "AlarmSettingsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_settings_dialog);
+    }
 
     @Override
     public void onClick(View v) {
