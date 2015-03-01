307ceac35400ca4b4ecb4cd33c6aedd436f2badf
AnimalRationale <wojciech.mardyla@gmail.com>
2015-03-01 23:31:47 +0100

    Cleanup.

--- app/src/main/java/pl/appnode/napwatch/AboutDialog.java
+++ app/src/main/java/pl/appnode/napwatch/AboutDialog.java
@@ -14,7 +14,7 @@ public class AboutDialog {
 
     private static String versionName;
     private static String versionCode;
-    private static Drawable aboutIcon;
+    private static Drawable versionIcon;
 
     private static void versionInfo(Context context) {
         try {
@@ -22,12 +22,12 @@ public class AboutDialog {
             PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
             versionName = info.versionName;
             versionCode = String.valueOf(info.versionCode);
-            aboutIcon = manager.getApplicationIcon("pl.appnode.napwatch");
+            versionIcon = manager.getApplicationIcon("pl.appnode.napwatch");
         }
         catch (PackageManager.NameNotFoundException ex) {
             versionName = context.getResources().getString(R.string.about_dialog_ver_name_err);
             versionCode = context.getResources().getString(R.string.about_dialog_ver_code_err);
-            aboutIcon = null;
+            versionIcon = null;
         }
     }
 
