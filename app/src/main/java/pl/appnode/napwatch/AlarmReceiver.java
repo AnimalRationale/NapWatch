3ee51d4ef069c411a345a6e95d96df5586215117
AnimalRationale <wojciech.mardyla@gmail.com>
2015-03-09 23:48:59 +0100

    Init for AlarmManager awake.

---
+++ app/src/main/java/pl/appnode/napwatch/AlarmReceiver.java
@@ -0,0 +1,14 @@
+package pl.appnode.napwatch;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent alarmIntent) {
+
+    }
+
+}
