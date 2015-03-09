ab412f759bca31048ad0f74dfd6e113faf02deb3
AnimalRationale <wojciech.mardyla@gmail.com>
2015-03-09 23:59:06 +0100

    WakeLock acquire with static method.

--- app/src/main/java/pl/appnode/napwatch/AlarmReceiver.java
+++ app/src/main/java/pl/appnode/napwatch/AlarmReceiver.java
@@ -3,9 +3,21 @@ package pl.appnode.napwatch;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
+    private static final String LOCK_TAG = "pl.appnode.napwatch";
+    private static PowerManager.WakeLock sWakeLock = null;
+
+    private static synchronized void acquireWakeLock (Context context) {
+        if (sWakeLock == null) {
+            PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            sWakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, LOCK_TAG);
+            sWakeLock.setReferenceCounted(true);
+        }
+        sWakeLock.acquire();
+    }
     @Override
     public void onReceive(Context context, Intent alarmIntent) {
 
