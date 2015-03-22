4f1a62b4365772a6b882ae2d99d14679c411f9cd
AnimalRationale <wojciech.mardyla@gmail.com>
2015-03-22 23:07:09 +0100

    Cleanup, init for proper values in widget.

--- app/src/main/java/pl/appnode/napwatch/NapWatchWidgetProvider.java
+++ app/src/main/java/pl/appnode/napwatch/NapWatchWidgetProvider.java
@@ -5,11 +5,16 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
 public class NapWatchWidgetProvider extends AppWidgetProvider {
 
+    public static final String ALARMS_PREFS_FILE = "AlarmsPrefsFile";
+
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        SharedPreferences alarmsPrefs = context.getSharedPreferences(ALARMS_PREFS_FILE, 0);
+        String alarmPrefix;
         final int N = appWidgetIds.length;
         for (int i = 0; i < N; i++) {
             int appWidgetId = appWidgetIds[i];
@@ -17,6 +22,10 @@ public class NapWatchWidgetProvider extends AppWidgetProvider {
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.napwatch_widget);
             views.setOnClickPendingIntent(R.id.widget_round_btn1, pendingIntent);
+            for (int j = 1; j <= 4; j++) {
+                alarmPrefix = "Alarm_" + j;
+
+            }
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
