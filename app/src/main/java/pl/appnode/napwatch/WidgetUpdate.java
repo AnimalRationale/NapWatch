402ec8a8bc65d45291ddf60b8b8e306261d19457
AnimalRationale <wojciech.mardyla@gmail.com>
2015-03-26 13:47:42 +0100

    Widget update refactor, cleanup.

---
+++ app/src/main/java/pl/appnode/napwatch/WidgetUpdate.java
@@ -0,0 +1,39 @@
+package pl.appnode.napwatch;
+
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import static pl.appnode.napwatch.StateConstants.WIDGET_BUTTONS;
+
+public class WidgetUpdate {
+
+    private static RemoteViews sWidgetViews = null;
+    private static ComponentName sWidget = null;
+    private static AppWidgetManager sWidgetManager = null;
+
+    private static void getWidget(Context context) {
+        sWidgetViews = new RemoteViews(context.getPackageName(), R.layout.napwatch_widget);
+        sWidget = new ComponentName(context, NapWatchWidgetProvider.class);
+        sWidgetManager = AppWidgetManager.getInstance(context);
+    }
+
+    public static void setButtonOn(int widgetButtonId, Context context) {
+        if (sWidgetViews == null) {getWidget(context);}
+        sWidgetViews.setInt(WIDGET_BUTTONS[widgetButtonId], "setBackgroundResource", R.drawable.round_button_selected);
+        sWidgetManager.updateAppWidget(sWidget, sWidgetViews);
+    }
+
+    public static void setButtonOff(int widgetButtonId, Context context) {
+        if (sWidgetViews == null) {getWidget(context);}
+        sWidgetViews.setInt(WIDGET_BUTTONS[widgetButtonId], "setBackgroundResource", R.drawable.round_button);
+        sWidgetManager.updateAppWidget(sWidget, sWidgetViews);
+    }
+
+    public static void buttonTime(int widgetButtonId, String timeToFinish, Context context) {
+        if (sWidgetViews == null) {getWidget(context);}
+        sWidgetViews.setTextViewText(WIDGET_BUTTONS[widgetButtonId], timeToFinish);
+        sWidgetManager.updateAppWidget(sWidget, sWidgetViews);
+    }
+}
