c4b4bc750c0a923056f2d827b0428aa895d1f2b3
AnimalRationale <wojciech.mardyla@gmail.com>
2015-03-27 12:02:55 +0100

    Cleanup, version bump.

--- app/src/main/java/pl/appnode/napwatch/MainActivity.java
+++ app/src/main/java/pl/appnode/napwatch/MainActivity.java
@@ -86,12 +86,7 @@ public class MainActivity extends Activity {
         saveSharedPrefs();
         unregisterReceiver(mCountDownBroadcast);
         Log.d(TAG, "OnPause unregistered broadcast receiver.");
-//        Intent intent = new Intent(this,NapWatchWidgetProvider.class);
-//        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-//        int ids[] = AppWidgetManager.getInstance(getApplication())
-//                .getAppWidgetIds(new ComponentName(getApplication(), NapWatchWidgetProvider.class));
-//        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,ids);
-//        sendBroadcast(intent);
+
     }
 
     @Override
