4fe889d297319849bfbae975e3244a29cb8a28c9
AnimalRationale <wojciech.mardyla@gmail.com>
2015-04-06 23:54:29 +0200

    Intent from widget initializing AlarmAdapter if app is not running.

--- app/src/main/java/pl/appnode/napwatch/MainActivity.java
+++ app/src/main/java/pl/appnode/napwatch/MainActivity.java
@@ -48,6 +48,14 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        if (intent.hasExtra("id")) {
+            if (mAA == null) {
+                mAA = new AlarmAdapter(createList(), MainActivity.this);
+                mAA.alarmAction(intent.getIntExtra("id", 1));
+                finish();
+            }
+        }
         setContentView(R.layout.activity_main);
         RecyclerView recList = (RecyclerView) findViewById(R.id.alarmList);
         recList.setHasFixedSize(true);
