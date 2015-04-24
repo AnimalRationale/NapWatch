20b10d0e8adac40cceccd48d174d843353f91949
AnimalRationale <wojciech.mardyla@gmail.com>
2015-04-24 22:43:48 +0200

    Showing ActivityOFF on lock screen.

--- app/src/main/java/pl/appnode/napwatch/FullscreenOffActivity.java
+++ app/src/main/java/pl/appnode/napwatch/FullscreenOffActivity.java
@@ -11,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.view.WindowManager;
 
 
 /**
@@ -51,6 +52,10 @@ public class FullscreenOffActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
         setContentView(R.layout.activity_fullscreen_off);
         setupActionBar();
