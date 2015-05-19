87a51951e2f6fb0072b9d27fd3220667fa64eca7
AnimalRationale <wojciech.mardyla@gmail.com>
2015-05-19 23:06:04 +0200

    Cleanup.

--- app/src/main/java/pl/appnode/napwatch/FullscreenOffActivity.java
+++ app/src/main/java/pl/appnode/napwatch/FullscreenOffActivity.java
@@ -139,7 +139,7 @@ public class FullscreenOffActivity extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.alarm_off_button).setOnTouchListener(mDelayHideTouchListener);
 
     }
 
