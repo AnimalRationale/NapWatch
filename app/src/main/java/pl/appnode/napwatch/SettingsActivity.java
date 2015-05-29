693d60a5d88d88c1ce8f463b91e4e2f396b31a4f
AnimalRationale <wojciech.mardyla@gmail.com>
2015-05-30 01:13:29 +0200

    Settings activity theme switch.

--- app/src/main/java/pl/appnode/napwatch/SettingsActivity.java
+++ app/src/main/java/pl/appnode/napwatch/SettingsActivity.java
@@ -1,11 +1,17 @@
 package pl.appnode.napwatch;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        if (settings.getBoolean("settings_checkbox_theme", false)) {
+            setTheme(android.R.style.Theme_DeviceDefault);
+        } else setTheme(android.R.style.Theme_DeviceDefault_Light);
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
     }
