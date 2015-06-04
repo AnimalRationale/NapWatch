fcb74b4435c2a847da036f9025145efa76d9c264
AnimalRationale <wojciech.mardyla@gmail.com>
2015-06-04 16:41:49 +0200

    ActionBar up button in settings activity.

--- app/src/main/java/pl/appnode/napwatch/SettingsActivity.java
+++ app/src/main/java/pl/appnode/napwatch/SettingsActivity.java
@@ -4,6 +4,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 import static pl.appnode.napwatch.ThemeSetup.themeSetup;
 
@@ -12,6 +14,17 @@ public class SettingsActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         themeSetup(this);
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
