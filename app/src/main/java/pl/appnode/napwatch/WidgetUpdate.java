5e80b38b9f72395ca102bd4301e66cc623395bc6
AnimalRationale <wojciech.mardyla@gmail.com>
2015-06-05 16:22:06 +0200

    Cleanup (WidgetUpdate).

--- app/src/main/java/pl/appnode/napwatch/WidgetUpdate.java
+++ app/src/main/java/pl/appnode/napwatch/WidgetUpdate.java
@@ -3,6 +3,7 @@ package pl.appnode.napwatch;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.widget.RemoteViews;
 
 import static pl.appnode.napwatch.StateConstants.WIDGET_BUTTONS;
@@ -19,22 +20,22 @@ public class WidgetUpdate {
         sWidgetManager = AppWidgetManager.getInstance(context);
     }
 
-    public static void setButtonOn(int widgetButtonId, Context context) {
+    private static void setButtonColor (int widgetButtonId, Context context, int background) {
         getWidget(context);
-        sWidgetViews.setInt(WIDGET_BUTTONS[widgetButtonId], "setBackgroundResource", R.drawable.round_button_pressed);
+        sWidgetViews.setInt(WIDGET_BUTTONS[widgetButtonId], "setBackgroundResource", background);
         sWidgetManager.updateAppWidget(sWidget, sWidgetViews);
+    }
+
+    public static void setButtonOn(int widgetButtonId, Context context) {
+        setButtonColor(widgetButtonId, context, R.drawable.round_button_pressed);
     }
 
     public static void setButtonFinish(int widgetButtonId, Context context) {
-        getWidget(context);
-        sWidgetViews.setInt(WIDGET_BUTTONS[widgetButtonId], "setBackgroundResource", R.drawable.round_button_selected);
-        sWidgetManager.updateAppWidget(sWidget, sWidgetViews);
+        setButtonColor(widgetButtonId, context, R.drawable.round_button_selected);
     }
 
     public static void setButtonOff(int widgetButtonId, Context context) {
-        getWidget(context);
-        sWidgetViews.setInt(WIDGET_BUTTONS[widgetButtonId], "setBackgroundResource", R.drawable.round_button);
-        sWidgetManager.updateAppWidget(sWidget, sWidgetViews);
+        setButtonColor(widgetButtonId, context, R.drawable.round_button);
     }
 
     public static void buttonTime(int widgetButtonId, String timeToFinish, Context context) {
