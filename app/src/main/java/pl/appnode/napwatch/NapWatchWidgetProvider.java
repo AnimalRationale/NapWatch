5c533ef6b78779e763fb6efb93d324d9f8375f2a
AnimalRationale <wojciech.mardyla@gmail.com>
2015-07-01 17:28:37 +0200

    Cleanup.

--- app/src/main/java/pl/appnode/napwatch/NapWatchWidgetProvider.java
+++ app/src/main/java/pl/appnode/napwatch/NapWatchWidgetProvider.java
@@ -24,9 +24,6 @@ import static pl.appnode.napwatch.StateConstants.WIDGET_BUTTON_ACTION;
 public class NapWatchWidgetProvider extends AppWidgetProvider {
 
     private static final String TAG = "AppWidgetProvider";
-    private static RemoteViews sWidgetViews = null;
-    private static ComponentName sWidget = null;
-    private static AppWidgetManager sWidgetManager = null;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -83,64 +80,6 @@ public class NapWatchWidgetProvider extends AppWidgetProvider {
                     context.startActivity(commandIntent);
                 }
             } else i++;
-        }
-    }
-
-    public static class WidgetUpdateService extends Service {
-
-        private int mStartMode = START_STICKY;
-        private int mOrientation;
-
-        @Override
-        public void onCreate() {
-            super.onCreate();
-        }
-
-        @Override
-        public int onStartCommand(Intent intent, int flags, int startId) {
-            mOrientation = this.getResources().getConfiguration().orientation;
-            Log.d(TAG, "WidgetUpdate Service.");
-            return mStartMode;
-        }
-
-        private static void getWidget(Context context) {
-            sWidgetViews = new RemoteViews(context.getPackageName(), R.layout.napwatch_widget);
-            sWidget = new ComponentName(context, NapWatchWidgetProvider.class);
-            sWidgetManager = AppWidgetManager.getInstance(context);
-        }
-
-        public void reassignWidgetButtons(Context context) {
-            getWidget(context);
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-            sWidgetViews.setOnClickPendingIntent(WIDGET_BUTTONS[0], pendingIntent);
-            Log.d(TAG, "WidgetUpdate Service reassigning app button.");
-            for (int i = 1; i <= 4; i++) {
-                sWidgetViews.setOnClickPendingIntent(WIDGET_BUTTONS[i], getPendingSelfIntent(context, WIDGET_BUTTON_ACTION[i]));
-                Log.d(TAG, "WidgetUpdate Service reassigning timer #" + i + " button.");
-            }
-        }
-
-        private PendingIntent getPendingSelfIntent(Context context, String action) {
-            Intent intent = new Intent(context, getClass());
-            intent.setAction(action);
-            Log.d(TAG, "WidgetUpdate Service pendingSelfIntent.");
-            return PendingIntent.getBroadcast(context, 0, intent, 0);
-        }
-
-        @Override
-        public void onConfigurationChanged(Configuration newConfig) {
-            Log.d(TAG, "WidgetUpdate Service configuration change.");
-            if(newConfig.orientation != mOrientation)
-            {
-                mOrientation = newConfig.orientation;
-                reassignWidgetButtons(this);
-            }
-        }
-
-        @Override
-        public IBinder onBind(Intent arg0) {
-            return null;
         }
     }
 
